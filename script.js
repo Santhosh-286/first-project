72965794f98f4a83ff005392f4a9ed9b7479f260
Santhosh-286 <sanbrosy286143@gmail.com>
2025-05-07 15:47:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function add() {
+    var side = document.getElementById("side");
+
+    side.style.display = "flex"
+}
+
+function add2() {
+    var side = document.getElementById("side");
+
+    side.style.display = "none"
+}
+
+
